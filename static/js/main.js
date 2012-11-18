b3f932d95284b543251646894effe42a6b80668e
Tim Baxter <tim.baxter@cerner.com>
2012-11-18 08:02:48 -0600

    general work

--- static/js/main.js
+++ static/js/main.js
@@ -9,22 +9,22 @@
 // Note that the child selector does not have to be a direct descendant.
 // It will toggle any matching children of the main selector.
 ********************************/
-$('a.trigger').each(function(e){
-	var hash = this.hash;
-	// check for child selector
-	if (hash.split(">").length > 1) {
-		if (document.width > 1000 ){
-			hash = hash.split(">")[0] + ' ' + hash.split(">")[1];
-		} else {
-			hash = hash.split(">")[0];
-		}
-	}
-	$(hash).hide();
-	$(this).click(function(e) {
-		e.preventDefault();
-		$(hash).toggle();
-		$(this).toggleClass('activated');
-	});
+$('a.trigger').each(function(){
+  var hash = this.hash;
+  // check for child selector
+  if (hash.split(">").length > 1) {
+    if (document.width > 1000 ){
+      hash = hash.split(">")[0] + ' ' + hash.split(">")[1];
+    } else {
+      hash = hash.split(">")[0];
+    }
+  }
+  $(hash).hide();
+  $(this).click(function(e) {
+    e.preventDefault();
+    $(hash).toggle();
+    $(this).toggleClass('activated');
+  });
 });
 
 // function(s) for collapsing/uncollapsing header onscroll
@@ -32,7 +32,7 @@ $('a.trigger').each(function(e){
 var collapsed = true;
 function getYOffset() {
     var pageY;
-    if(typeof(window.pageYOffset)=='number') {
+    if(typeof(window.pageYOffset) === 'number') {
        pageY=window.pageYOffset;
     }
     else {
@@ -41,12 +41,98 @@ function getYOffset() {
     return pageY;
 }
 $(window).scroll(function () {
-	if (getYOffset() > 120 && collapsed === false) {
-		collapsed = true;
-		$('body').addClass('collapsed');
-	}
-	if (getYOffset() < 80 && collapsed === true) {
-		collapsed = false;
-		$('body').removeClass('collapsed');
-	}
+  if (getYOffset() > 120 && collapsed === false) {
+    collapsed = true;
+    $('body').addClass('collapsed');
+  }
+  if (getYOffset() < 80 && collapsed === true) {
+    collapsed = false;
+    $('body').removeClass('collapsed');
+  }
 });
+
+
+/* tabs can be used two ways:
+  With inside navigation:
+    container.tabs
+      ul
+      tab block
+      tab block
+  or with remote navigation
+    ul.remote_tabs_tabbed.nav(anywhere on the page
+    container.tabs
+      tab block
+      tab block
+  
+  within ul, a.active denotes active tab.
+  If no active tab is set, the first will be the default.
+
+  A tab block can be any block level element other than a UL, so div, article, section, etc will all work.
+  UL is reserved for nav. If you need a UL, wrap it.
+
+  tabit respects page hash, so foo.com/#bar
+  will go to the #bar tab
+  
+  You must set the container class to .tabs.
+  When tabs are activated, it will be given the class .tabbed,
+  so you can style it appropriately and only style blocks that have been tabbed.
+  
+  And remember to call tabit()
+ */
+function tabit() {
+  $('.tabs').each(function() {
+    var tabbed       = $(this),
+        active       = '';
+
+    tabbed.addClass('tabbed');
+    tabbed.sections  = $(this).find('> *').not('ul');
+    tabbed.nav       = tabbed.find('> ul');
+    var nav_height   = tabbed.nav.height();
+
+    if (tabbed.nav.length === 0) {
+        tabbed.nav= $('.remote_tabs_nav');
+        nav_height = 0;
+      }
+    if (!tabbed.nav) {
+        console.log('could not find tab nav!');
+        return;
+      }
+    //tabbed.sections.hide(); // Should be done in css, but fallback for safety.
+    tabbed.navitems = tabbed.nav.find('li');
+    //console.log('parent: '+ tabbed.parent().attr('id'))
+    //console.log('navitems size: '+ tabbed.navitems.size())
+    
+    if (window.location.hash) {
+        active = window.location.hash.replace('-view','');
+        if (tabbed.nav) {
+          tabbed.navitems.removeClass('active');
+          tabbed.nav.find('a[href='+active+']').parent().addClass('active');
+        }
+      } else { // no hash available. check if one was pre-defined.
+        active = tabbed.nav.find('li.active a').hash;
+        if (!active) { // or set it to first item
+          active = tabbed.navitems.eq(0).addClass('active').find('a').get(0).hash;
+        }
+      }
+    $(active).addClass('activeTab');
+    
+    tabbed.nav.find('a').click(function(e) {
+      e.preventDefault();
+      var target          = this.hash,
+          tabbed_sections = $(target).parent().find('> *').not('ul');
+
+      window.location.hash = target + '-view';
+      parent = $(this).parent();
+      parent.addClass('active');
+      var navitems = parent.parent().find('li');
+      $(navitems).not(parent).removeClass('active');
+      $(target).addClass('activeTab');
+      // ensure the container height will be at least the target height...
+      // it'll make the css much easier
+      tabbed.css('min-height', $(target).height() + nav_height + 'px');
+      tabbed_sections.not(target).removeClass('activeTab');
+      //set_asset_offset();
+    });
+  });
+}
+tabit();
