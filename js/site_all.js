9b581ee812e4d0fbfcfd3e5c3b4df0b62c614f65
Tim Baxter <tim.baxter@cerner.com>
2013-09-16 12:57:02 -0500

    Refactor

--- js/site_all.js
+++ js/site_all.js
@@ -1,6 +1,13 @@
 /*************************************************************
-* Combines common_libs and capo_combined from capo
-* with site specific js for one output file.
+* Combines all necessary js to create site-combined-min.js
+* Includes:
+*
+* Jquery 1.10.1
+* Pickadate.js (2.0)
+* Hammer.js (for touch gestures)
+* Stickit
+* A modified version of refineSlide (for photo galleries)
+* main.js
 *
 **************************************************************/
 
